7303bcfca10565a2e34f00dad49f84712df4e0ae
CarlosSandoval2003 <jawicho@outlook.com>
2022-02-05 14:56:15 -0600

    Ingreso de nota

---
+++ Revision Notas/RevisionNotas.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Nota;
+    cout << "Ingrese la nota obtenida:"  << endl;
+    cin >> Nota;
+    cout << Nota;
+
+
+
+
+  return 0;
+}
+
