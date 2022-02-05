233f20f8abef0feb1da8cd8094fa128ce4e9b48a
CarlosSandoval2003 <jawicho@outlook.com>
2022-02-05 16:05:42 -0600

    Segundo case (nota mayor o igual a 60)

--- Revision Notas/RevisionNotas.cpp
+++ Revision Notas/RevisionNotas.cpp
@@ -10,10 +10,17 @@ int main()
     if (Nota < 60){
         Nota = 0;
     }
+    else if (Nota >= 60){
+        Nota = 1;
+    }
     switch(Nota)
     {
     case 0:
         cout << "Has perdido la materia :(";
+        break;
+    case 1:
+        cout << "Has ganado la materia, pero necesitas mejorar :/";
+        break;
     }
 
 
