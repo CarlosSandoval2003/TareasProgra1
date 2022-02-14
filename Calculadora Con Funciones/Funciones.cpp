f53dc4893a710ce16d2d643bca114fa05a12c3d1
CarlosSandoval2003 <jawicho@outlook.com>
2022-02-14 17:07:38 -0600

    Calculadora con Funciones

--- Calculadora Con Funciones/Funciones.cpp
+++ Calculadora Con Funciones/Funciones.cpp
@@ -2,35 +2,58 @@
 
 using namespace std;
 
-int main()
-{
+    char operacion;
+    int cierremenu = 0;
+    int cierre = 0;
     int num1 ;
     int num2 ;
     int res ;
-    char operacion;
-    cout << "Escoja una operacion (+,-,*,/)" << endl;
-    cin >> operacion;
-    if (operacion != '+' & operacion != '-' & operacion != '*' & operacion != '/' ){
-        cout << "Ingrese una operacion valida";
-        return 0;
+
+    void menu(){
+
+        if (operacion == '0'){
+            cierre = 2;
+            cierremenu = 2;
+        }
+        else if (operacion != '+' & operacion != '-' & operacion != '*' & operacion != '/'){
+        cout << "Ingrese una operacion valida" << endl;
+        }
+
+
+
     }
-    cout << "Ingrese el primer numero:";
-    cin >> num1;
-    cout << "Ingrese el segundo numero:";
-    cin >> num2;
-    if (operacion=='+')
-        res = num1 + num2;
-    else if (operacion == '-')
-        res = num1 - num2;
-    else if (operacion == '*')
-        res =num1 * num2;
-    else if (operacion == '/')
-        res = num1/num2;
+int main()
+{
+    while (cierremenu < 1){
+        cout << "Escoja una operacion (+,-,*,/)" << endl;
+        cout << "Si desea salir ingrese '0'" << endl;
+        cin >> operacion;
+        menu();
+        while (cierre < 1){
+            cout << "Ingrese el primer numero:";
+            cin >> num1;
+            cout << "Ingrese el segundo numero:";
+            cin >> num2;
+            if (operacion=='+')
+                res = num1 + num2;
+            else if (operacion == '-')
+                res = num1 - num2;
+            else if (operacion == '*')
+                res =num1 * num2;
+            else if (operacion == '/')
+                res = num1/num2;
+            cout << "El resultado es " << res << endl;
+            cierre = 2;
 
-    cout << "El resultado es " << res;
+        }
+        cierre = 0;
+    }
 
 
-
-  return 0;
+    return 0;
 }
+
+
+
+
 
